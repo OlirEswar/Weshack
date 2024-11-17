a20d3ebfe02d079fa48b50b99ec9472f3a36b0cf
Tommy Voelker <tommyvoelker@Tommys-MacBook-Pro-631.local>
2024-11-16 21:00:40 -0500

    sql output given input vars

--- api/index.py
+++ api/index.py
@@ -2,6 +2,7 @@ import psycopg2
 from psycopg2 import sql
 from dotenv import load_dotenv
 import os
+import random
 
 from flask import Flask, jsonify
 load_dotenv()
@@ -9,6 +10,14 @@ app = Flask(__name__)
 
 @app.route("/api/python")
 def hello_world():
+    name = "Joey"
+    prev_paper_topic = "math"
+    liked = 1
+    clicked = 1
+    timespent = 50
+    id = 0
+    sum = 0
+    
     connection = psycopg2.connect(
             dbname=os.environ["POSTGRES_DATABASE"],
             user=os.environ["POSTGRES_USER"],
@@ -20,12 +29,52 @@ def hello_world():
     # Create a cursor object
     cursor = connection.cursor()
 
+    query = sql.SQL(f"SELECT * FROM users where name='{name}';")
+    cursor.execute(query)
+    scores_arr = (cursor.fetchall())[0]
+
+    scores = {
+            "physics": scores_arr[1],
+            "math": scores_arr[2],
+            "cs": scores_arr[3],
+            "quantbio": scores_arr[4],
+            "quantfin": scores_arr[5],
+            "ee": scores_arr[6],
+            "stats": scores_arr[7],
+            "econ": scores_arr[8]
+        }
+    
+    for i in range(len(scores_arr)):
+        if i != 0:
+            sum += scores_arr[i]
+      
+    if liked == 1:
+        scores[prev_paper_topic] += 1
+    if clicked == 1:
+        scores[prev_paper_topic] += 2
+    #figure out log function for time spent
+
+    q_string = f"""UPDATE users
+                        SET {prev_paper_topic} = {scores[prev_paper_topic]} 
+                        """
+
+    query = sql.SQL(f"""UPDATE users
+                        SET {prev_paper_topic} = {scores[prev_paper_topic]} 
+                        """)
+    cursor.execute(query)
+    
+    next_topic = random.choices(list(scores.keys()), list(scores.values()))[0]
+    query = sql.SQL(f"SELECT * FROM papers where topics='{next_topic}';")
+    cursor.execute(query)
+    connection.commit()
+    next_paper = (cursor.fetchall())
+
     # Prepare and execute the SQL query to fetch all data
-    query = sql.SQL("SELECT * FROM todos")
+    query = sql.SQL(f"SELECT title FROM papers where id={id};")
     cursor.execute(query)
 
-    # Fetch all rows from the executed query
+    #Fetch all rows from the executed query
     rows = cursor.fetchall()
      
-    return jsonify(rows)
+    return jsonify(next_paper)
 
